a386d6fa5464771a421b32ef142c5e64a4f2718d
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-02-21 08:31:03 -0500

    changes

--- interactive/assignment03/sketch.js
+++ interactive/assignment03/sketch.js
@@ -4,17 +4,40 @@ let pharahSprite;
 //player
 let pharah;
 
+//enemies!!!!!
+let enemy1sprite;
+let enemy2sprite;
+let enemy3sprite;
+
+//enemy array
+let enemyArr;
+
+let images;
+let enemyNum;
+//NOTES
+//238 - ground (238 from the bottom, SO = height - 238);
+
 //preload assets
 function preload()
 {
   pharahSprite = loadImage("images/pharahSprite_small.png");
   bg = loadImage("images/bg.png");
+
+  //enemies, potentially variable with more effort lol
+  enemy1sprite = loadImage("images/enemy1.png");
+  enemy2sprite = loadImage("images/enemy2.png");
+  enemy3sprite = loadImage("images/enemy3.png");
+
 }
 
 function setup()
 {
   createCanvas(1920,1200);
   pharah = new Pharah();
+  enemy1 = new BadBoi(150, height-238-enemy1sprite.height, enemy1sprite, .03);
+  enemy2 = new BadBoi(400, height-238-enemy2sprite.height, enemy2sprite, .01);
+  enemy3 = new BadBoi(150, height-238-enemy3sprite.height, enemy3sprite, .02);
+  enemyArr = [enemy1, enemy2, enemy3];
 }
 
 function draw()
@@ -22,6 +45,11 @@ function draw()
   background(bg);
   pharah.display();
   pharah.move();
+  for (let i = 0; i < enemyArr.length; i++)
+  {
+    enemyArr[i].display();
+    enemyArr[i].move();
+  }
 }
 
 class Pharah
@@ -43,21 +71,25 @@ class Pharah
   move()
   {
     // move left?
-    if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
+    if (keyIsDown(LEFT_ARROW) || keyIsDown(65))
+    {
       // subtract from character's xSpeed
       this.xSpeed -= this.accel;
     }
     // move right?
-    if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
+    if (keyIsDown(RIGHT_ARROW) || keyIsDown(68))
+    {
       // add to character's xSpeed
       this.xSpeed += this.accel;
     }
     // move up?
-    if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
+    if (keyIsDown(UP_ARROW) || keyIsDown(87))
+    {
       // subtract from character's ySpeed
       this.ySpeed -= this.accel;
     }
-    if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
+    if (keyIsDown(DOWN_ARROW) || keyIsDown(83))
+    {
       // add to character's ySpeed
       this.ySpeed += this.accel;
     }
@@ -93,11 +125,25 @@ class BadBoi
 
     this.sprite = image;
     this.xDest = random(100, width-100);
+    this.speed = speed;
   }
 
   display()
   {
     image(this.sprite, this.xPos, this.yPos);
+  }
+  move()
+  {
+    let xDist = this.xDest - this.xPos;
+    this.xPos += this.speed * xDist;
 
+    if (dist(this.xPos, this.yPos, this.xDest, height-238) < 20)
+    {
+      shoot();
+    }
+  }
+  shoot()
+  {
+    console.log("pew pew!");
   }
 }
