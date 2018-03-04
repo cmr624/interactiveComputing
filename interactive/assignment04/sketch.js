66abcb09f8ee5af0ece488acb5becc1b961fec7f
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-03-04 13:02:52 -0500

    almost completely done

--- interactive/assignment04/sketch.js
+++ interactive/assignment04/sketch.js
@@ -1,24 +1,34 @@
 
 const moles = [];
 let april;
+let golden;
+let donkey;
+let state;
+let cursorImg;
 
+let cursor;
 //scoreboard
 let scoreboard;
 let hits;
 let misses;
 let timer;
 
-let seconds;
+let clicked;
 
 function preload()
 {
   april = loadImage("images/aprilcircle.png");
+  golden = loadImage("images/goldenApril.png");
+  donkey = loadImage("images/donkey.png");
+  cursorImg = loadImage('images/cursor.png');
 }
 function setup()
 {
-  seconds = 0;
+  clicked = false;
+  state = 0;
   hits = 0;
   misses = 0;
+  timer = 30;
   createCanvas(500,500);
   const rowNum = 3;
   const colNum = 3;
@@ -30,28 +40,69 @@ function setup()
     }
   }
   scoreboard = new Scoreboard();
+  cursor = new Cursor();
 }
 
 function draw()
 {
+  checkTimer();
+  if (state === 0)
+  {
+    menu();
+  }
+  else if (state === 1)
+  {
+    game();
+  }
+  else if (state === 2)
+  {
+    gameOver();
+  }
+}
+
+function menu()
+{
+  background(255);
+  text("hey", 50, 50);
+}
+//adapted from: http://alpha.editor.p5js.org/marynotari/sketches/S1T2ZTMp-
+//mainly because millis() caused me *actual* nightmares
+function checkTimer()
+{
+  if (frameCount % 60 === 0 && timer > 0)
+  {
+    timer--;
+  }
+  if (timer <= 0)
+  {
+    state = 2;
+  }
+}
+
+function mouseClicked()
+{
+  clicked = true;
+}
+
+function game()
+{
   background(0);
   //console.log("millis" + millis());
-  timer = millis() / 1000;
-  timer = Math.floor(timer);
   scoreboard.display();
   for (let i = 0; i < moles.length; i++)
   {
     moles[i].display();
     moles[i].update();
   }
+  cursor.display();
+}
+function gameOver()
+{
+  background(255);
+  text("bye", 50, 50);
 }
 class Scoreboard
 {
-  //scoreboard
-  /*
-  let hits;
-  let misses;
-  let timer;*/
   constructor()
   {
     this.xPos = 10;
@@ -66,6 +117,19 @@ class Scoreboard
     text("Timer: " + timer, this.xPos, this.yPos + (2 * this.interval));
   }
 }
+
+class Cursor
+{
+  constructor()
+  {
+    this.sprite = cursorImg;
+  }
+  display()
+  {
+    noCursor();
+    image(this.sprite, mouseX, mouseY);
+  }
+}
 class Mole
 {
   constructor(x, y)
@@ -76,8 +140,14 @@ class Mole
     this.r = 100;
     this.border = 10;
     this.april = april;
-    this.number = random(2, 10);
-    this.lastTime = 0;
+    this.lowerBound = 0;
+    this.upperBound = 10;
+    this.number = random(this.lowerBound, this.upperBound);
+    this.random = Math.floor(random(this.lowerBound, this.upperBound));
+    this.isGolden = false;
+    this.isDonkey = false;
+    this.golden = golden;
+    this.donkey = donkey;
   }
   display()
   {
@@ -85,7 +155,18 @@ class Mole
     {
       fill(255);
       ellipse(this.xPos, this.yPos, this.r, this.r);
-      image(this.april, this.xPos - this.april.width/2, this.yPos - this.april.height / 2);
+      if (this.isGolden)
+      {
+        image(this.golden, this.xPos - this.april.width/2, this.yPos - this.april.height / 2);
+      }
+      else if (this.isDonkey)
+      {
+        image(this.donkey, this.xPos - this.april.width/2, this.yPos - this.april.height / 2);
+      }
+      else
+      {
+        image(this.april, this.xPos - this.april.width/2, this.yPos - this.april.height / 2);
+      }
     }
     else
     {
@@ -97,11 +178,47 @@ class Mole
   }
   update()
   {
-    if (this.currentTime > this.number)
+    //adapted from: http://alpha.editor.p5js.org/marynotari/sketches/S1T2ZTMp-
+    if (frameCount % 60 === 0 && this.number > 0)
     {
-      this.lastTime = this.currentTime;
-      this.currentTime = (millis() / 1000) - this.lastTime;
+      this.number--;
+    }
+    if (this.number <= 0)
+    {
+      if (this.random === 3)
+      {
+        this.isGolden = true;
+      }
+      else if (this.random === 5)
+      {
+        this.isDonkey = true;
+      }
       this.flip();
+      this.number = random(this.lowerBound, this.upperBound);
+    }
+
+    if (this.checkHit())
+    {
+      if (this.visible)
+      {
+        if (this.isGolden)
+        {
+          hits += 10;
+        }
+        else if (this.isDonkey)
+        {
+          misses += 10;
+        }
+        else
+        {
+          hits += 1;
+        }
+        this.flip();
+      }
+      else
+      {
+        misses += 1;
+      }
     }
   }
   flip()
@@ -109,10 +226,27 @@ class Mole
     if (this.visible)
     {
       this.visible = false;
+      this.isGolden = false;
+      this.isDonkey = false;
     }
     else
     {
       this.visible = true;
     }
   }
+
+  checkHit()
+  {
+    let distanceCheck =(dist(this.xPos, this.yPos, mouseX, mouseY) < this.r);
+    if (distanceCheck && clicked)
+    {
+      clicked = false;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
 }
